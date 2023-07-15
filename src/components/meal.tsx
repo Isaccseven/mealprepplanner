c781cc9ffa936440a06c9c6272bebd0765376415
Luca Henn <luca-henn@t-online.de>
2023-07-15 03:17:39 +0200

    feat(image-upload):

--- src/components/meal.tsx
+++ src/components/meal.tsx
@@ -1,68 +1,98 @@
-// Meal.tsx
-import { useDrag, useDrop, DropTargetMonitor } from 'react-dnd';
-import { useRef } from 'react';
+import { DropTargetMonitor, useDrag, useDrop } from "react-dnd";
+import { useRef } from "react";
+import Image from "next/image";
 
 interface DragItem {
-    index: number;
-    weekday: string;
-    type: string;
+  index: number;
+  weekday: string;
+  type: string;
 }
 
 interface MealProps {
-    mealName: string;
-    index: number;
-    weekday: string;
-    moveMeal: (fromDay: string, toDay: string, fromIndex: number, toIndex: number) => void;
-    deleteMeal: (weekday: string, index: number) => void;
+  mealName: string;
+  index: number;
+  weekday: string;
+  image: File | null;
+  moveMeal: (
+    fromDay: string,
+    toDay: string,
+    fromIndex: number,
+    toIndex: number,
+  ) => void;
+  deleteMeal: (weekday: string, index: number) => void;
 }
 
-const Meal: React.FC<MealProps> = ({ mealName, index, weekday, moveMeal, deleteMeal }) => {
-    const ref = useRef<HTMLDivElement>(null);
+const Meal: React.FC<MealProps> = ({
+  mealName,
+  index,
+  weekday,
+  image,
+  moveMeal,
+  deleteMeal,
+}) => {
+  const ref = useRef<HTMLDivElement>(null);
 
-    const [{ isDragging }, drag] = useDrag({
-        type: 'meal',
-        item: { index, weekday } as DragItem,
-        collect: (monitor) => ({
-            isDragging: monitor.isDragging(),
-        }),
-    });
+  const [{ isDragging }, drag] = useDrag({
+    type: "meal",
+    item: { index, weekday } as DragItem,
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
 
-    const [, drop] = useDrop<DragItem, {}, {}>({
-        accept: 'meal',
-        hover(item: DragItem, monitor: DropTargetMonitor) {
-            if (!ref.current) {
-                return;
-            }
+  const [, drop] = useDrop<DragItem, {}, {}>({
+    accept: "meal",
+    hover(item: DragItem, monitor: DropTargetMonitor) {
+      if (!ref.current) {
+        return;
+      }
 
-            const dragIndex = item.index;
-            const hoverIndex = index;
-            const dragDay = item.weekday;
-            const hoverDay = weekday;
+      const dragIndex = item.index;
+      const hoverIndex = index;
+      const dragDay = item.weekday;
+      const hoverDay = weekday;
 
-            if (dragIndex === hoverIndex && dragDay === hoverDay) {
-                return;
-            }
+      if (dragIndex === hoverIndex && dragDay === hoverDay) {
+        return;
+      }
 
-            moveMeal(dragDay, hoverDay, dragIndex, hoverIndex);
-            item.index = hoverIndex;
-            item.weekday = hoverDay;
-        },
-    });
+      moveMeal(dragDay, hoverDay, dragIndex, hoverIndex);
+      item.index = hoverIndex;
+      item.weekday = hoverDay;
+    },
+  });
 
-    drag(drop(ref));
+  drag(drop(ref));
 
-    const handleDeleteMeal = () => {
-        deleteMeal(weekday, index);
-    };
+  const handleDeleteMeal = () => {
+    deleteMeal(weekday, index);
+  };
 
-    return (
-        <div className="m-3 bg-white  rounded p-2 flex items-center" ref={ref} style={{ opacity: isDragging ? 0.5 : 1 }}>
-            <div className="flex-grow">{mealName}</div>
-            <button className="ml-2 bg-blue-500 hover:bg-blue-600 px-2 py-1 text-white rounded" onClick={handleDeleteMeal}>
-                X
-            </button>
+  return (
+    <div
+      className="m-3 bg-white rounded p-2 flex items-center"
+      ref={ref}
+      style={{ opacity: isDragging ? 0.5 : 1 }}
+    >
+      {image && (
+        <div className="mr-2">
+          <Image
+            src={URL.createObjectURL(image)}
+            width={50}
+            height={50}
+            alt="Picture of the recipe"
+          />
         </div>
-    );
-}
+      )}
+      <div className="flex-grow text-black">{mealName}</div>
+      <button
+        className="ml-2 bg-blue-500 hover:bg-blue-600 px-2 py-1 text-white rounded"
+        onClick={handleDeleteMeal}
+      >
+        X
+      </button>
+    </div>
+  );
+};
 
 export default Meal;
--- src/components/planning_board.tsx
+++ src/components/planning_board.tsx
@@ -1,88 +1,142 @@
-// PlanningBoard.tsx
 import Meal from "@/components/meal";
 import { useDrop } from "react-dnd";
 import { useState } from "react";
 
+interface MealData {
+  mealName: string;
+  mealPhoto: File | null;
+}
+
 interface PlanningBoardProps {
-    weekday: string;
-    meals: string[];
-    moveMeal: (fromDay: string, toDay: string, fromIndex: number, toIndex: number) => void;
-    addMeal: (day: string, mealName: string) => void;
-    deleteMeal: (weekday: string, index: number) => void;
+  weekday: string;
+  meals: MealData[]; // Update the type to MealData[]
+  moveMeal: (
+    fromDay: string,
+    toDay: string,
+    fromIndex: number,
+    toIndex: number,
+  ) => void;
+  addMeal: (day: string, mealName: string, mealPhoto: File) => void; // Update the addMeal function signature
+  deleteMeal: (weekday: string, index: number) => void;
 }
 
 interface DragItem {
-    index: number;
-    type: string;
-    weekday: string;
+  index: number;
+  type: string;
+  weekday: string;
 }
 
-export default function PlanningBoard({ weekday, meals, moveMeal, addMeal, deleteMeal }: PlanningBoardProps) {
-    const [, drop] = useDrop<DragItem, {}, {}>({
-        accept: 'meal',
-        drop(item: DragItem) {
-            if (item.weekday !== weekday) {
-                moveMeal(item.weekday, weekday, item.index, meals.length);
-            }
-            return {};  // Return an empty object
-        },
-    });
+export default function PlanningBoard({
+  weekday,
+  meals,
+  moveMeal,
+  addMeal,
+  deleteMeal,
+}: PlanningBoardProps) {
+  const [, drop] = useDrop<DragItem, {}, {}>({
+    accept: "meal",
+    drop(item: DragItem) {
+      if (item.weekday !== weekday) {
+        moveMeal(item.weekday, weekday, item.index, meals.length);
+      }
+      return {}; // Return an empty object
+    },
+  });
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [newMealName, setNewMealName] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [newMealName, setNewMealName] = useState("");
+  const [mealPhoto, setMealPhoto] = useState<File | null>(null);
 
-    const handleNewMealNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setNewMealName(event.target.value);
-    };
+  const handleNewMealNameChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setNewMealName(event.target.value);
+  };
 
-    const handleAddMeal = () => {
-        if (newMealName.trim() !== '') {
-            addMeal(weekday, newMealName);
-            setNewMealName('');
-            setIsModalOpen(false);
-        }
-    };
+  const handleMealPhotoChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setMealPhoto(event.target.files[0]);
+    }
+  };
 
-    const handleModalOpen = () => {
-        setIsModalOpen(true);
-    };
+  const handleAddMeal = () => {
+    if (newMealName.trim() !== "" && mealPhoto !== null) {
+      addMeal(weekday, newMealName, mealPhoto);
+      setNewMealName("");
+      setMealPhoto(null);
+      setIsModalOpen(false);
+    }
+  };
 
-    const handleModalClose = () => {
-        setIsModalOpen(false);
-    };
+  const handleModalOpen = () => {
+    setIsModalOpen(true);
+  };
 
-    return (
-        <div ref={drop} className="flex-grow bg-gray-300 text-gray-800 m-3 w-1/6 text-center rounded-md">
-            <div>
-                <button className="m-3 bg-blue-500 hover:bg-blue-600 px-4 py-2 text-white rounded" onClick={handleModalOpen}>
-                    Add Meal
-                </button>
-                {isModalOpen && (
-                    <div className="fixed inset-0 flex items-center justify-center">
-                        <div className="bg-white flex flex-col justify-evenly rounded-lg p-8">
-                            <input
-                                className={"m-2 p-2 border-4 border-dotted border-gray-400 text-center"}
-                                type="text"
-                                value={newMealName}
-                                onChange={handleNewMealNameChange}
-                                placeholder="Enter meal name"
-                            />
-                            <button className={"m-2 bg-gray-300 rounded"} onClick={handleAddMeal}>Add</button>
-                            <button className={"m-2 bg-gray-300 rounded"} onClick={handleModalClose}>Cancel</button>
-                        </div>
-                    </div>
-                )}
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div
+      ref={drop}
+      className="flex-grow bg-gray-300 text-gray-800 m-3 w-1/6 text-center rounded-md"
+    >
+      <div>
+        <button
+          className="m-3 bg-blue-500 hover:bg-blue-600 px-4 py-2 text-white rounded"
+          onClick={handleModalOpen}
+        >
+          Add Meal
+        </button>
+        {isModalOpen && (
+          <div className="fixed inset-0 flex items-center justify-center">
+            <div className="bg-white flex flex-col justify-evenly rounded-lg p-8">
+              <input
+                className={
+                  "m-2 p-2 border-4 border-dotted border-gray-400 text-center"
+                }
+                type="text"
+                value={newMealName}
+                onChange={handleNewMealNameChange}
+                placeholder="Enter meal name"
+              />
+              <input
+                className={
+                  "m-2 p-2 border-4 border-dotted border-gray-400 text-center"
+                }
+                type="file"
+                accept="image/*"
+                onChange={handleMealPhotoChange}
+              />
+              <button
+                className={"m-2 bg-gray-300 rounded"}
+                onClick={handleAddMeal}
+              >
+                Add
+              </button>
+              <button
+                className={"m-2 bg-gray-300 rounded"}
+                onClick={handleModalClose}
+              >
+                Cancel
+              </button>
             </div>
-            {meals.map((meal, index) => (
-                <Meal
-                    key={index}
-                    mealName={meal}
-                    moveMeal={moveMeal}
-                    weekday={weekday}
-                    index={index}
-                    deleteMeal={deleteMeal}
-                />
-            ))}
-        </div>
-    );
+          </div>
+        )}
+      </div>
+      {meals.map((meal, index) => (
+        <Meal
+          key={index}
+          mealName={meal.mealName}
+          index={index}
+          weekday={weekday}
+          image={meal.mealPhoto}
+          moveMeal={moveMeal}
+          deleteMeal={deleteMeal}
+        />
+      ))}
+    </div>
+  );
 }
